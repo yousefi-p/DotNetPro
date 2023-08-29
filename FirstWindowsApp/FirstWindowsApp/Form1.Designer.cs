8c5b2dc70c54af127f46b25a8d21c915e378ba59
Pouya <yousefi-p@users.noreply.github.com>
2023-08-29 20:22:25 +0330

    New Features

--- FirstWindowsApp/FirstWindowsApp/Form1.Designer.cs
+++ FirstWindowsApp/FirstWindowsApp/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            CellPhoneTxtBox = new TextBox();
+            saveBtn = new Button();
+            Cell = new Label();
+            OutputTxtBox = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 15);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.Yes;
+            label1.Size = new Size(64, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Cell Phone";
+            // 
+            // CellPhoneTxtBox
+            // 
+            CellPhoneTxtBox.Location = new Point(105, 12);
+            CellPhoneTxtBox.Name = "CellPhoneTxtBox";
+            CellPhoneTxtBox.Size = new Size(212, 23);
+            CellPhoneTxtBox.TabIndex = 1;
+            // 
+            // saveBtn
+            // 
+            saveBtn.Location = new Point(25, 41);
+            saveBtn.Name = "saveBtn";
+            saveBtn.Size = new Size(292, 42);
+            saveBtn.TabIndex = 2;
+            saveBtn.Text = "Save";
+            saveBtn.UseVisualStyleBackColor = true;
+            saveBtn.Click += saveBtn_Click;
+            // 
+            // Cell
+            // 
+            Cell.AutoSize = true;
+            Cell.Location = new Point(25, 103);
+            Cell.Name = "Cell";
+            Cell.Size = new Size(68, 15);
+            Cell.TabIndex = 0;
+            Cell.Text = "Cell Output";
+            // 
+            // OutputTxtBox
+            // 
+            OutputTxtBox.Location = new Point(105, 100);
+            OutputTxtBox.Name = "OutputTxtBox";
+            OutputTxtBox.Size = new Size(212, 23);
+            OutputTxtBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(343, 155);
+            Controls.Add(saveBtn);
+            Controls.Add(OutputTxtBox);
+            Controls.Add(CellPhoneTxtBox);
+            Controls.Add(Cell);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox CellPhoneTxtBox;
+        private Button saveBtn;
+        private Label Cell;
+        private TextBox OutputTxtBox;
     }
 }
--- FirstWindowsApp/FirstWindowsApp/Form1.cs
+++ FirstWindowsApp/FirstWindowsApp/Form1.cs
@@ -6,5 +6,27 @@ namespace FirstWindowsApp
         {
             InitializeComponent();
         }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            string inputCellPhone = CellPhoneTxtBox.Text;
+            if (inputCellPhone.Length == 11 && int.Parse(inputCellPhone[0].ToString()) == 0)
+            {
+                OutputTxtBox.Text = inputCellPhone;
+
+            }
+            else if (inputCellPhone.Length == 13 && (inputCellPhone[0].Equals('+') && inputCellPhone[1].Equals('9') && inputCellPhone[2].Equals('8'))
+            {
+                OutputTxtBox.Text = '0'+inputCellPhone.Substring(3);
+            }
+            else if (inputCellPhone.Length == 10 && inputCellPhone[0].Equals('9'))
+            {
+
+            }
+            else
+            {
+                OutputTxtBox.Text = "Worng Number";
+            }
+        }
     }
 }
