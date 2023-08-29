c9dc0e1709b858122172d386614179a4036d3aae
Pouya <yousefi-p@users.noreply.github.com>
2023-08-29 20:39:00 +0330

    change to substring method

--- FirstWindowsApp/FirstWindowsApp/Form1.cs
+++ FirstWindowsApp/FirstWindowsApp/Form1.cs
@@ -10,14 +10,18 @@ namespace FirstWindowsApp
         private void saveBtn_Click(object sender, EventArgs e)
         {
             string inputCellPhone = CellPhoneTxtBox.Text;
-            if (inputCellPhone.Length == 11 && int.Parse(inputCellPhone[0].ToString()) == 0)
+            if (inputCellPhone.Length == 11 && inputCellPhone[0].Equals('0'))
             {
                 OutputTxtBox.Text = inputCellPhone;
 
             }
-            else if (inputCellPhone.Length == 13 && (inputCellPhone[0].Equals('+') && inputCellPhone[1].Equals('9') && inputCellPhone[2].Equals('8'))
+            else if (inputCellPhone.Length == 13 && inputCellPhone.Substring(0,3).Equals("+98"))
             {
                 OutputTxtBox.Text = '0'+inputCellPhone.Substring(3);
+            }
+            else if (inputCellPhone.Length == 14 && inputCellPhone.Substring(0, 4).Equals("0098"))
+            {
+                OutputTxtBox.Text = inputCellPhone.Replace("0098", "0");
             }
             else
             {
